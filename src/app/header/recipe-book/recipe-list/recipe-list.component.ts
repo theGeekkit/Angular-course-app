a6453a9dfdc21a3da0692322ce92a5631dec6d7f
theGeekkit <sonofgondolin16@gmail.com>
2022-09-25 16:15:43 -0500

    9-25-22

--- src/app/header/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/header/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { recipe } from 'src/app/header/recipe-book/recipe/recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: recipe[] = [];
 
   constructor() { }
 
